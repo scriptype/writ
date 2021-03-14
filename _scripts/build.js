431fe89dc0bafe3b763eca0e33c7b57180b0f923
scriptype <scriptyper@gmail.com>
2021-03-14 16:48:38 +0200

    Refactor & Improve build script

--- _scripts/build.js
+++ _scripts/build.js
@@ -11,29 +11,161 @@ const EXCLUDED_PATHS = new RegExp(settings.ignorePaths.join('|'))
 
 const readFileContent = (path) => fs.readFileSync(path, { encoding: 'utf-8' })
 
-const isContentDirectory = (path) => {
+const isTargetDirectory = (path) => {
   const isDirectory = fs.lstatSync(path).isDirectory()
   const isNotMeta = !path.includes('_') && !path.includes('.')
   return isDirectory && isNotMeta
 }
 
-const mkSiteDir = () => {
+const createSiteDir = () => {
   if (SITE_DIR === '.' || SITE_DIR === '/' || SITE_DIR === '~') {
     throw new Error(`Dangerous export directory: "${SITE_DIR}". Won't continue.`)
   }
   try {
     execSync(`rm -rf ${SITE_DIR}`)
   } catch (e) {
-    console.log('mkSiteDir error:', e)
+    console.log('createSiteDir error:', e)
   } finally {
     fs.mkdirSync(SITE_DIR)
   }
 }
 
-const cpPaths = (paths) => {
+const copyPaths = (paths) => {
   paths
     .filter(p => !p.match(EXCLUDED_PATHS))
     .forEach(path => execSync(`cp -R ${path} ${SITE_DIR}`))
+}
+
+const createPostsJSON = ({ path, posts }) => {
+  const postsJSON = posts.map(({ content, output, ...rest }) => rest)
+  fs.writeFileSync(path, JSON.stringify(postsJSON, null, 2))
+  console.log('created:', path)
+}
+
+const getTargetDirectories = (paths) => {
+  return paths
+    .filter(isTargetDirectory)
+    .map(slug => {
+      const paths = fs.readdirSync(slug)
+      if (!paths.includes(CATEGORY_INFO_FILE)) {
+        return {
+          name: '',
+          slug,
+        }
+      }
+      const info = require(`../${slug}/${CATEGORY_INFO_FILE}`)
+      return {
+        name: info.name,
+        slug: slug,
+        permalink: `/${slug}`,
+        isCategory: true
+      }
+    })
+}
+
+const compileNonCategoryDirectories = (directories) => {
+  directories.forEach(dir => {
+    fs.readdirSync(`${dir.slug}`)
+      .filter(p => p.match(/.hbs$/))
+      .forEach(fileName => {
+        const inPath = `${dir.slug}/${fileName}`
+        const outPath = `${SITE_DIR}/${inPath.replace('.hbs', '.html')}`
+        compileTemplate({
+          content: readFileContent(inPath),
+          path: outPath,
+          data: {
+            site: settings.site
+          }
+        })
+        fs.rmSync(outPath.replace('.html', '.hbs'))
+      })
+  })
+}
+
+const compileTemplate = ({ content, path, data }) => {
+  const template = Handlebars.compile(content)
+  const output = template(data || {})
+  fs.writeFileSync(path, output)
+  console.log('created:', path)
+  return output
+}
+
+const compileCategoryPosts = (categories) => {
+  return categories.reduce((acc, category) => {
+    const categoryPostDirs = fs.readdirSync(category.slug).filter(d => isTargetDirectory(`${category.slug}/${d}`))
+    const categoryPosts = []
+    categoryPostDirs.forEach(postDir => {
+      fs.readdirSync(`${category.slug}/${postDir}`)
+        .filter(p => p.match(/.hbs$/))
+        .forEach(postFilePath => {
+          const { output, content } = compilePost({
+            path: `${category.slug}/${postDir}/${postFilePath}`,
+            data: {
+              site: settings.site,
+              category
+            }
+          })
+          if (postFilePath === 'index.hbs') {
+            categoryPosts.push(
+              parsePostData({
+                content,
+                output,
+                category,
+                postDir
+              })
+            )
+          }
+        })
+    })
+
+    categoryPosts.sort((a, b) => b.publishedAt - a.publishedAt)
+
+    return {
+      ...acc,
+      [category.slug]: categoryPosts
+    }
+  }, {})
+}
+
+const compileCategoryIndexes = ({ categories, posts }) => {
+  categories.forEach(category => {
+    compileTemplate({
+      content: '{{>category}}',
+      path: `${SITE_DIR}/${category.slug}/index.html`,
+      data: {
+        site: settings.site,
+        category,
+        posts: posts[category.slug]
+      }
+    })
+  })
+}
+
+const compilePost = ({ path, data }) => {
+  const content = readFileContent(path)
+  const newPath = path.replace(/\.hbs$/, '.html')
+  const output = compileTemplate({
+    content,
+    path: `${SITE_DIR}/${newPath}`,
+    data
+  })
+  fs.rmSync(`${SITE_DIR}/${path}`)
+  return {
+    output,
+    content
+  }
+}
+
+const compileIndex = ({ categories, posts }) => {
+  compileTemplate({
+    content: '{{>index}}',
+    path: `${SITE_DIR}/index.html`,
+    data: {
+      site: settings.site,
+      posts,
+      categories
+    }
+  })
 }
 
 const parsePostData = ({ content, output, category, postDir }) => {
@@ -53,35 +185,6 @@ const parsePostData = ({ content, output, category, postDir }) => {
     output,
     category
   }
-}
-
-const compileTemplate = ({ content, path, data }) => {
-  const template = Handlebars.compile(content)
-  const output = template(data || {})
-  fs.writeFileSync(path, output)
-  console.log('created:', path)
-  return output
-}
-
-const compilePost = ({ path, data }) => {
-  const content = readFileContent(path)
-  const newPath = path.replace(/\.hbs$/, '.html')
-  const output = compileTemplate({
-    content,
-    path: `${SITE_DIR}/${newPath}`,
-    data
-  })
-  fs.rmSync(`${SITE_DIR}/${path}`)
-  return {
-    output,
-    content
-  }
-}
-
-const createPostsJSON = ({ path, posts }) => {
-  const postsJSON = posts.map(({ content, output, ...rest }) => rest)
-  fs.writeFileSync(path, JSON.stringify(postsJSON, null, 2))
-  console.log('created:', path)
 }
 
 const helpers = {
@@ -109,85 +212,31 @@ fs.readdirSync('./_partials').forEach(path => {
 })
 
 const allPaths = fs.readdirSync('.')
+const targetDirectories = getTargetDirectories(allPaths)
+const categories = targetDirectories.filter(dir => dir.isCategory)
+const nonCategoryDirectories = targetDirectories.filter(dir => !dir.isCategory)
 
-mkSiteDir()
-cpPaths(allPaths)
+createSiteDir()
+copyPaths(allPaths)
+compileNonCategoryDirectories(nonCategoryDirectories)
 
-const categories = allPaths
-  .filter(isContentDirectory)
-  .map(categorySlug => {
-    const paths = fs.readdirSync(categorySlug)
-    if (!paths.includes(CATEGORY_INFO_FILE)) {
-      return {
-        name: '',
-        slug: categorySlug,
-        visible: false
-      }
-    }
-    const info = require(`../${categorySlug}/${CATEGORY_INFO_FILE}`)
-    return {
-      name: info.name,
-      slug: categorySlug,
-      permalink: `/${categorySlug}`,
-      visible: true
-    }
-  })
+const postsByCategories = compileCategoryPosts(categories)
+compileCategoryIndexes({
+  categories,
+  posts: postsByCategories
+})
 
-const posts = categories.reduce((acc, category) => {
-  const categoryPostDirs = fs.readdirSync(category.slug).filter(d => isContentDirectory(`${category.slug}/${d}`))
-  const categoryPosts = []
-  categoryPostDirs.forEach(postDir => {
-    fs.readdirSync(`${category.slug}/${postDir}`)
-      .filter(p => p.match(/.hbs$/))
-      .forEach(postFilePath => {
-        const { output, content } = compilePost({
-          path: `${category.slug}/${postDir}/${postFilePath}`,
-          data: {
-            site: settings.site,
-            category
-          }
-        })
-        if (category.visible && postFilePath === 'index.hbs') {
-          categoryPosts.push(
-            parsePostData({
-              content,
-              output,
-              category,
-              postDir
-            })
-          )
-        }
-      })
-  })
+const posts = Object.keys(postsByCategories)
+  .reduce((acc, categorySlug) => {
+    return [
+      ...acc,
+      ...postsByCategories[categorySlug]
+    ]
+  }, [])
 
-  categoryPosts.sort((a, b) => b.publishedAt - a.publishedAt)
-
-  if (category.visible) {
-    compileTemplate({
-      content: '{{>category}}',
-      path: `${SITE_DIR}/${category.slug}/index.html`,
-      data: {
-        site: settings.site,
-        category,
-        posts: categoryPosts
-      }
-    })
-  }
-
-  return [
-    ...acc,
-    ...categoryPosts
-  ]
-}, [])
-
-compileTemplate({
-  content: '{{>index}}',
-  path: `${SITE_DIR}/index.html`,
-  data: {
-    site: settings.site,
-    posts,
-    categories: categories.filter(c => c.visible)
-  }
+compileIndex({
+  categories,
+  posts
 })
 
 createPostsJSON({
