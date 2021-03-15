4a7f1a8bf02aee75dcfa8d779ddf203139ceb5ef
scriptype <scriptyper@gmail.com>
2021-03-16 00:32:42 +0200

    Render category in text post only if it exists

--- assets/scripts/text-post.js
+++ assets/scripts/text-post.js
@@ -12,15 +12,19 @@
     dateCell.innerHTML = dateIcon.cloneNode(true).outerHTML
     dateCell.title = dateAltText
 
-    const categoryAltText = categoryCell.textContent
-    const categoryIcon = categoryIconTmpl.content.firstElementChild
-    categoryCell.innerHTML = categoryIcon.cloneNode(true).outerHTML
-    categoryCell.title = categoryAltText
+    if (categoryCell) {
+      const categoryAltText = categoryCell.textContent
+      const categoryIcon = categoryIconTmpl.content.firstElementChild
+      categoryCell.innerHTML = categoryIcon.cloneNode(true).outerHTML
+      categoryCell.title = categoryAltText
+    }
 
-    const tagIcon = tagIconTmpl.content.firstElementChild
-    tagCells.forEach(cell => {
-      cell.innerHTML = tagIcon.cloneNode(true).outerHTML
-    })
+    if (tagCells) {
+      const tagIcon = tagIconTmpl.content.firstElementChild
+      tagCells.forEach(cell => {
+        cell.innerHTML = tagIcon.cloneNode(true).outerHTML
+      })
+    }
   }
 
   function initMusicListIcons(musicListItems, { musicListIconTmpl }) {
