484a894d20e5e4edc5c7157d94876d296b41d2ee
scriptype <scriptyper@gmail.com>
2022-11-10 22:04:45 +0200

    Delete _cms/core (for new npm pkg)

--- _cms/core/targets/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  compilePosts: require('./post').compile,
-  compileHomepage: require('./homepage').compile,
-  compileCategoryPages: require('./category-page').compile,
-  compileSubPages: require('./subpage').compile,
-  compilePostsJSON: require('./posts-json').compile
-}
