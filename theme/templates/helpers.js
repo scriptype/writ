f81f092ecb3ccc303a39d199f54f161b5f0a42cc
scriptype <scriptyper@gmail.com>
2024-01-30 12:08:14 +0300

    Build

--- theme/templates/helpers.js
+++ theme/templates/helpers.js
@@ -1,12 +1,6 @@
 module.exports = {
-  multiLineTextList(list) {
-    if (typeof list === 'string') {
-      return list
-    }
-    return list
-      .map(s => s.trim()).filter(Boolean)
-      .map(s => `<li>${s}</li>`)
-      .join('\n')
+  region(name) {
+    return ` data-region-id="${name}" `
   },
 
   seeMore() {
