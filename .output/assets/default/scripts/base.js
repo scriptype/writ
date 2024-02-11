141065579d8d46273836a6572c680dbb04166e8f
scriptype <scriptyper@gmail.com>
2024-02-12 02:23:51 +0300

    Include .output back in git

---
+++ .output/assets/default/scripts/base.js
@@ -0,0 +1,17 @@
+function initDarkMode(darkToggle) {
+  const getDarkMemory = () => JSON.parse(localStorage.getItem('dark'))
+
+  darkToggle.addEventListener('click', () => {
+    document.body.classList.toggle('dark')
+    const darkMemory = getDarkMemory()
+    localStorage.setItem('dark', !darkMemory)
+  })
+
+  document.body.classList.toggle('dark', getDarkMemory())
+}
+
+const UI = {
+  darkToggle: document.querySelector('.site-title')
+}
+
+initDarkMode(UI.darkToggle)
